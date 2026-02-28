e93cc8f00807b5dec456d842cc7ac80f26ff0815
Taghreed M. Kamel <taghreedMKamel@gmail.com>
2026-02-28 21:26:10 +0200

    Install laravel breeze for login, registration and profile links

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+require __DIR__.'/auth.php';
